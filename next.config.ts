ba50118d4d268abbfa8b51fa07c6a07c83a96067
NiharikaSaxena18 <imt_2023052@iiitm.ac.in>
2026-03-03 23:00:29 +0530

    base-path-set

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/your-repo-name",
+  assetPrefix: "/your-repo-name/",
 };
 
 export default nextConfig;
