dfd92c6f5e9d6c91551fe6f17e1a28dcd9192567
NiharikaSaxena18 <imt_2023052@iiitm.ac.in>
2026-04-07 21:21:12 +0530

    admin-panel

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // output: "export",
-  // images: {
-  //   unoptimized: true,
-  // },
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
   turbopack: {
     root: process.cwd(),
   },
-  // basePath: "/Germane-Synergy",
-  // assetPrefix: "/Germane-Synergy/",
+  basePath: "/Germane-Synergy",
+  assetPrefix: "/Germane-Synergy/",
 };
 
 export default nextConfig;
